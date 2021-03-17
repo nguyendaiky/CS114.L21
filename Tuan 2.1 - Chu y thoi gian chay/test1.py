9db81b684c0af17212af5560d28e0c7159627d9e
nguyendaiky <daiky2579@gmail.com>
2021-03-18 00:29:44 +0700

    upload modified file

--- Tuan 2.1 - Chu y thoi gian chay/test1.py
+++ Tuan 2.1 - Chu y thoi gian chay/test1.py
@@ -1,11 +1,25 @@
-h,w = [int(x) for x in input().split()]
-
-zeroLine = '0'
-for i in range(w-1):
-    zeroLine = zeroLine + ' 0'
-
-image = ''
+h, w = map(int,input().split())
+array = ''
 for i in range(h):
-    image = image + input()[0:w*2-1] + ' '
+    row = input()[0:(w*2-1)]
+    array += row + ' '
+top, left, bottom, right = [int(x) for x in input().split()]
 
-print(image)
+zeroRow = '0'
+for i in range(w-1):
+    zeroRow += ' 0'
+for r in range(h):
+    if r < top or r > bottom or top > bottom or left > right:
+        print(zeroRow)
+    else:
+        print(zeroRow[0:(left*2)], end='')
+
+        begin = r*w*2 + left*2
+        end = begin + (right-left)*2+2
+        roi = array[begin:end].split()
+        for i in roi:
+            print(i, end= ' ')
+
+        print(zeroRow[(right*2+2):(w*2-1)])
+
+
--- Tuan 2.1 - Chu y thoi gian chay/test3.py
+++ Tuan 2.1 - Chu y thoi gian chay/test3.py
@@ -1,3 +1,7 @@
 
-a,b = map(int,input().split())
-print(a+b)
+import cv2
+import matplotlib as plt
+
+path = ''
+img = cv2.imread(path,1)
+cv2.imshow(img)
